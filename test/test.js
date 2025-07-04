2562b525e0bf8dba5bba9cdc9ffbc57815266dee
Sven Helmberger <sven.helmberger@dataciders.com>
2025-07-04 12:12:47 +0200

    Initiziale Js project
    Made yarn init the project and added dev dependencies.
    
    We don't really run per se and have no normal dependencies. jscodeshift
    uses us, not the other way around.
    
    For the tests, we use jscodeshift to, well, test.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import { describe, it } from "mocha"
+import assert from "assert"
+
+describe("Testing", function(){
+    it("Mocha is integrated", function()
+    {
+        assert(true);
+    });
+});
