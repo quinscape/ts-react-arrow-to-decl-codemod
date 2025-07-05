7c8ba2656582961e34002bbf97733de842f7086b
Sven Helmberger <sven.helmberger@dataciders.com>
2025-07-05 15:30:46 +0200

    Send cli test output to log
    Keep our test logs clean by redirecting the cli test output to a log
    file

--- test/cli-test.js
+++ test/cli-test.js
@@ -2,17 +2,24 @@ const assert = require("assert")
 const fs = require("fs")
 const sh = require("shelljs")
 
+const logFile = "test/cli-test-log.txt"
+
 describe("Transform script", function(){
     it("works via actual jscodeshift CLI", function()
     {
-        before(() => sh.cp("test/Test.tsx.bak", "test/Test.tsx"))
+        before(
+            () => {
+                sh.cp("test/Test.tsx.bak", "test/Test.tsx")
+                sh.rm(logFile)
+            }
+        )
 
-        sh.exec("./node_modules/.bin/jscodeshift -t index.js test/Test.tsx")
+        sh.exec("./node_modules/.bin/jscodeshift -t index.js test/Test.tsx > " + logFile)
 
         const source = fs.readFileSync("test/Test.tsx", "utf8")
         const expected = fs.readFileSync("test/Test-expected.tsx", "utf8")
 
-        assert.equal(source, expected, "Output of transform does not match contents of Test-expected.tsx");
+        assert.equal(source, expected, "Output of transform does not match contents of Test-expected.tsx. See " + logFile + " for details.");
 
     });
 });
